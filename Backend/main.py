57f33a1e5b78a8744e26456f82d4383dfa161969
Alvin Li <limanwah0924@gmail.com>
2025-05-15 10:52:04 -0500

    Add CORS middleware

--- Backend/main.py
+++ Backend/main.py
@@ -36,7 +36,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["https://sqlvalidator.onrender.com/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
